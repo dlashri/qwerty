ad5fd3308fc640c055f0bf34f7d83bb663008f90
Dima <vika.dyra.10@mail.ru>
2020-12-23 17:56:36 +0300

    lab1

---
+++ index_4.php
@@ -0,0 +1,29 @@
+<?php
+class A
+{
+}
+class B extends A
+{
+    public function __construct($a)
+    {
+        $this->a =$a;
+    }
+    protected $a;
+}
+class C extends B
+{
+    public function __construct($a,$b,$c)
+    {
+        $this->b = $b;
+        $this->c = $c;
+        parent::__construct($a);
+    }
+    protected $b;
+    protected $c;
+}
+
+$a1 = new A();
+$a2 = new A();
+$a3 = new A();
+$b4 = new B($a2);
+$c5 = new С($a1,$a3,$b4);
